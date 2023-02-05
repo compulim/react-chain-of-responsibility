ee69787150b1b10ebbbb3a6ed1be5b50cbdfe0d6
William Wong <compulim@hotmail.com>
2023-02-05 01:36:30 -0800

    Move proxyTransform to Proxy component

--- packages/use-render/src/createComponentStrategy.tsx
+++ packages/use-render/src/createComponentStrategy.tsx
@@ -6,38 +6,22 @@ import type { ComponentMiddleware } from './types';
 import type { PropsWithChildren } from 'react';
 
 type ProviderContext<P extends {}, T extends {} = P> = {
-  useComponent: (props: P) => ComponentType<T> | false | null | undefined;
+  useComponent: (props: P) => ComponentType<P | T> | false | null | undefined;
 };
 
 type ProviderProps<P extends {}, S = undefined, T extends {} = P> = PropsWithChildren<{
   init?: S;
-  middleware?: ComponentMiddleware<P, S, T>[];
+  middleware?: ComponentMiddleware<P, S, P | T>[];
 }>;
 
-export default function createComponentStrategy<P extends {}, S = undefined, T extends {} = P>(options: {
-  transformProps: (props: P) => T;
-}): {
-  Provider: ComponentType<ProviderProps<P, S, T>>;
-  Proxy: ComponentType<P>;
-  useComponent: () => (props: P) => ComponentType<T> | false | null | undefined;
+type ProxyProps<P extends {}, T extends {} = P> = P & {
+  proxyTransform?: (props: P) => T;
 };
 
-export default function createComponentStrategy<P extends {}, S = undefined>(
-  options?: Omit<{}, 'transformProps'>
-): {
-  Provider: ComponentType<ProviderProps<P, S, P>>;
-  Proxy: ComponentType<P>;
-  useComponent: () => (props: P) => ComponentType<P> | false | null | undefined;
-};
-
-export default function createComponentStrategy<P extends {}, S = undefined, T extends {} = P>({
-  transformProps
-}: {
-  transformProps?: (props: P) => T;
-} = {}): {
-  Provider: ComponentType<ProviderProps<P, S, T>>;
-  Proxy: ComponentType<P>;
-  useComponent: () => (props: P) => ComponentType<T> | false | null | undefined;
+export default function createComponentStrategy<P extends {}, S = undefined, T extends {} = P>(): {
+  Provider: ComponentType<ProviderProps<P, S, P | T>>;
+  Proxy: ComponentType<ProxyProps<P, T>>;
+  useComponent: () => (props: P) => ComponentType<P | T> | false | null | undefined;
 } {
   const context = createContext<ProviderContext<P, T> | undefined>(undefined);
 
@@ -47,14 +31,14 @@ export default function createComponentStrategy<P extends {}, S = undefined, T e
         // We are reversing because it is easier to read:
         // - With reverse, [a, b, c] will become a(b(c(fn)))
         // - Without reverse, [a, b, c] will become c(b(a(fn)))
-        applyMiddleware<[P], ComponentType<T> | false | null | undefined, [S | undefined]>(
+        applyMiddleware<[P], ComponentType<P | T> | false | null | undefined, [S | undefined]>(
           ...[...(middleware || [])].reverse()
         )(init)(() => null),
       [init, middleware]
     );
 
     // TODO: Add way to verify that every middleware is returning things correctly.
-    const useComponent = useCallback<(props: P) => ComponentType<T> | false | null | undefined>(
+    const useComponent = useCallback<(props: P) => ComponentType<P | T> | false | null | undefined>(
       (props: P) => enhancer(props),
       [enhancer]
     );
@@ -74,13 +58,18 @@ export default function createComponentStrategy<P extends {}, S = undefined, T e
     return contextValue.useComponent;
   };
 
-  const Proxy: ComponentType<P> = (props: P) => {
-    const Component = useComponent()(props);
+  const Proxy: ComponentType<ProxyProps<P, T>> = (props: ProxyProps<P, T>) => {
+    const { proxyTransform } = props;
+    const propsWithoutproxyTransform = { ...props };
+
+    delete propsWithoutproxyTransform.proxyTransform;
+
+    const Component = useComponent()(propsWithoutproxyTransform);
 
     // TypeScript limitation.
-    // If `transformProps` is not defined, `T` must be same as `P`, we defined this as one of the overload.
+    // If `proxyTransform` is not defined, `T` must be same as `P`, we defined this as one of the overload.
     // As `T = P`, we can cast `props: P` to `T`.
-    const transformedProps = transformProps ? transformProps(props) : (props as unknown as T);
+    const transformedProps = proxyTransform ? proxyTransform(props) : props;
 
     return Component ? <Component {...transformedProps} /> : null;
   };
