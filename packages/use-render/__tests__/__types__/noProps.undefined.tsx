6e61f3385de08a406e4aaad7ae2042d3e1c9e0dd
William Wong <compulim@hotmail.com>
2023-03-19 16:47:22 -0700

    Remove unrelated tests

--- packages/use-render/__tests__/__types__/noProps.undefined.tsx
+++
@@ -1,8 +0,0 @@
-import { wrapWith } from 'react-wrap-with';
-import React from 'react';
-
-import type { PropsWithChildren } from 'react';
-
-const Header = ({ children }: PropsWithChildren<{}>) => <h1>{children}</h1>;
-
-wrapWith(Header, undefined);
