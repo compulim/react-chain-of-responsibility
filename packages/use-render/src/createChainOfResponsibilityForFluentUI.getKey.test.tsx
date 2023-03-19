4575ae94fa2df3c5894b9aa9cd9dfc9a93d058a7
William Wong <compulim@hotmail.com>
2023-03-18 20:54:01 -0700

    Add test

---
+++ packages/use-render/src/createChainOfResponsibilityForFluentUI.getKey.test.tsx
@@ -0,0 +1,60 @@
+/** @jest-environment jsdom */
+/// <reference types="@types/jest" />
+
+import { render } from '@testing-library/react';
+import React, { Fragment, useMemo } from 'react';
+
+import createChainOfResponsibilityForFluentUI from './createChainOfResponsibilityForFluentUI';
+
+type Props = { value?: number };
+
+test('useRenderFunctionCallback should call getKey() for computing "key" attribute', () => {
+  // GIVEN: A middleware.
+  const { Provider, types, useRenderFunctionCallback } = createChainOfResponsibilityForFluentUI<Props>();
+
+  const keys: (null | number | string | undefined)[] = [];
+  const getKey = jest.fn(() => 'a');
+
+  const Inner = () => {
+    const renderFunction = useRenderFunctionCallback({ getKey });
+
+    const element = renderFunction({ value: 1 });
+
+    keys.push(element?.key);
+
+    return element;
+  };
+
+  const App = () => {
+    const middleware = useMemo<(typeof types.middleware)[]>(
+      () => [
+        () =>
+          () =>
+          () =>
+          ({ value }) =>
+            <Fragment>{value}</Fragment>
+      ],
+      []
+    );
+
+    return (
+      <Provider middleware={middleware}>
+        <Inner />
+      </Provider>
+    );
+  };
+
+  // WHEN: Render.
+  const result = render(<App />);
+
+  // THEN: It should render 1.
+  expect(result.container).toHaveProperty('textContent', '1');
+
+  // THEN: It should have called getKey() once with { value: 1 } and returned "a".
+  expect(getKey).toHaveBeenCalledTimes(1);
+  expect(getKey).toHaveBeenNthCalledWith(1, { value: 1 });
+  expect(getKey).toHaveNthReturnedWith(1, 'a');
+
+  // THEN: It should have rendered 1 key.
+  expect(keys).toEqual(['a']);
+});
