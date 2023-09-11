98671b2c019e5f07cc2d71684e1e8ec1718f9924
William Wong <compulim@hotmail.com>
2023-09-10 22:07:47 -0700

    Meaningful integration test

--- packages/integration-test/requireDefault.test.mjs
+++ packages/integration-test/requireDefault.test.mjs
@@ -4,9 +4,52 @@ const {
   createChainOfResponsibility,
   createChainOfResponsibilityForFluentUI
 } = require('react-chain-of-responsibility');
+const { create } = require('react-test-renderer');
+const React = require('react');
+
+const { Fragment } = React;
+
+const HelloWorld = () => <Fragment>Hello, World!</Fragment>;
 
 test('simple scenario', () => {
-  // TODO: Non-inclusive.
-  expect(typeof createChainOfResponsibility).toBe('function');
-  expect(typeof createChainOfResponsibilityForFluentUI).toBe('function');
+  // GIVEN: A middleware return a component that would render "Hello, World!".
+  const { Provider, Proxy } = createChainOfResponsibility();
+
+  // WHEN: Render <Proxy>.
+  const App = () => (
+    <Provider middleware={[() => () => () => HelloWorld]}>
+      <Proxy />
+    </Provider>
+  );
+
+  const renderer = create(<App />);
+
+  // THEN: It should render "Hello, World!".
+  expect(renderer.toJSON()).toMatchInlineSnapshot(`"Hello, World!"`);
+});
+
+test('Fluent UI scenario', () => {
+  // GIVEN: A middleware.
+  const { Provider, useBuildRenderFunction } = createChainOfResponsibilityForFluentUI();
+
+  const DisplayString = ({ text }) => <Fragment>{text}</Fragment>;
+  const CallRender = ({ text }) => useBuildRenderFunction()({ text }, HelloWorld);
+
+  const App = ({ text }) => (
+    <Provider middleware={[() => next => request => (request?.text ? DisplayString : next(request))]}>
+      <CallRender text={text} />
+    </Provider>
+  );
+
+  // WHEN: Render with "Aloha!" as "text" prop.
+  const renderer = create(<App text="Aloha!" />);
+
+  // THEN: It should render "Aloha!".
+  expect(renderer.toJSON()).toMatchInlineSnapshot(`"Aloha!"`);
+
+  // WHEN: Re-render without "text" prop.
+  renderer.update(<App />);
+
+  // THEN: It should render "Hello, World!", which is the default render.
+  expect(renderer.toJSON()).toMatchInlineSnapshot(`"Hello, World!"`);
 });
