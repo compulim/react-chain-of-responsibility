1724ed6eaa981f811c707f44badcab314ba13150
William Wong <compulim@hotmail.com>
2024-03-31 05:57:32 -0700

    Use older way to render

--- packages/pages/src/app/index.tsx
+++ packages/pages/src/app/index.tsx
@@ -1,8 +1,17 @@
+// import React from 'react';
+// import { createRoot } from 'react-dom/client';
+
+// import App from './App';
+
+// const rootElement = document.getElementById('root');
+
+// rootElement && createRoot(rootElement).render(<App />);
+
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import { render } from 'react-dom';
 
 import App from './App';
 
 const rootElement = document.getElementById('root');
 
-rootElement && createRoot(rootElement).render(<App />);
+render(<App />, rootElement);
