9aab7faf8f44ad08b7def53953c503a3220ed8ea
William Wong <compulim@users.noreply.github.com>
2024-05-30 03:13:16 -0700

    Adapt

--- packages/react-chain-of-responsibility/tsup.config.ts
+++ packages/react-chain-of-responsibility/tsup.config.ts
@@ -4,7 +4,8 @@ export default defineConfig([
   {
     dts: true,
     entry: {
-      'react-chain-of-responsibility': './src/index.ts'
+      'react-chain-of-responsibility': './src/index.ts',
+      'react-chain-of-responsibility.fluentUI': './src/index.fluentUI.ts'
     },
     format: ['cjs', 'esm'],
     sourcemap: true
