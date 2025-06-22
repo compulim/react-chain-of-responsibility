86b63369ec7f05f3d44d850ecbaf5cf695de9b23
William Wong <compulim@hotmail.com>
2025-06-22 01:48:40 -0700

    For React 16

--- packages/pages/src/app2/index.tsx
+++ packages/pages/src/app2/index.tsx
@@ -4,22 +4,23 @@ if (typeof IS_DEVELOPMENT !== 'undefined' && IS_DEVELOPMENT) {
   new EventSource('/esbuild').addEventListener('change', () => location.reload());
 }
 
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
-
-const rootElement = document.getElementById('root');
-
-rootElement && createRoot(rootElement).render(<App />);
-
 // import React from 'react';
-// // Testing against multiple version of React.
-// // eslint-disable-next-line react/no-deprecated
-// import { render } from 'react-dom';
-
+// import { createRoot } from 'react-dom/client';
 // import App from './App.tsx';
+// import './index.css';
 
 // const rootElement = document.getElementById('root');
 
-// render(<App />, rootElement);
+// rootElement && createRoot(rootElement).render(<App />);
+
+import React from 'react';
+import './index.css';
+// Testing against multiple version of React.
+// eslint-disable-next-line react/no-deprecated
+import { render } from 'react-dom';
+
+import App from './App.tsx';
+
+const rootElement = document.getElementById('root');
+
+render(<App />, rootElement);
