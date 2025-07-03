e89173d65121380f0b0f8e4cf65f2f27e2ec5acd
William Wong <compulim@hotmail.com>
2025-07-03 00:06:30 -0700

    Fix ESLint

--- packages/react-chain-of-responsibility/src/index.fluentUI.ts
+++ packages/react-chain-of-responsibility/src/index.fluentUI.ts
@@ -1,4 +1,5 @@
 // eslint-disable-next-line import/no-deprecated
 import createChainOfResponsibilityForFluentUI from './createChainOfResponsibilityForFluentUI.tsx';
 
+// eslint-disable-next-line import/no-deprecated
 export { createChainOfResponsibilityForFluentUI };
