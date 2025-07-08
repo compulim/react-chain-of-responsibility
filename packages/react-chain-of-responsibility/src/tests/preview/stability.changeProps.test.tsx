85c9705ce9b76e96b2e022fa7b5feb07c4e0d9fe
William Wong <compulim@hotmail.com>
2025-07-08 15:45:21 -0700

    Add tests

---
+++ packages/react-chain-of-responsibility/src/tests/preview/stability.changeProps.test.tsx
@@ -0,0 +1,66 @@
+/** @jest-environment jsdom */
+/// <reference types="@types/jest" />
+
+import { scenario } from '@testduet/given-when-then';
+import { render } from '@testing-library/react';
+import React, { Fragment, memo } from 'react';
+
+import createChainOfResponsibility from '../../createChainOfResponsibilityAsRenderCallback';
+
+type Props = { readonly children?: never };
+
+scenario('stability test with changing props', bdd => {
+  bdd
+    .given('a TestComponent using chain of responsiblity', () => {
+      const {
+        Provider,
+        reactComponent,
+        types: _types,
+        useBuildRenderCallback
+      } = createChainOfResponsibility<number, Props>();
+
+      const renderCall = jest.fn();
+
+      const MyComponent = memo(function MyComponent(_: Props & { readonly dummy: string }) {
+        renderCall();
+
+        return <Fragment>Hello, World!</Fragment>;
+      });
+
+      const middleware: readonly (typeof _types.middleware)[] = [
+        // With a changing props, the <MyComponent> should be re-rendered.
+        () => () => () => reactComponent(MyComponent, () => ({ dummy: crypto.randomUUID() }))
+      ];
+
+      function MyProxy({ request }: { readonly request: number }) {
+        // We are using `useBuildRenderCallback` for less memoization.
+        return useBuildRenderCallback()(request)?.({});
+      }
+
+      return [
+        function TestComponent({ request }: { readonly request: number }) {
+          return (
+            <Provider middleware={middleware}>
+              <MyProxy request={request} />
+            </Provider>
+          );
+        },
+        renderCall
+      ] as const;
+    })
+    .when('the component is rendered', ([TestComponent]) => render(<TestComponent request={1} />))
+    .then('textContent should match', (_, { container }) =>
+      expect(container).toHaveProperty('textContent', 'Hello, World!')
+    )
+    .and('should have been rendered once', ([_, renderCall]) => expect(renderCall).toHaveBeenCalledTimes(1))
+    .when('the component is re-rendered with a different request', ([TestComponent], result) => {
+      // With same request, it should still re-render because props changed.
+      result.rerender(<TestComponent request={1} />);
+
+      return result;
+    })
+    .then('textContent should match', (_, { container }) =>
+      expect(container).toHaveProperty('textContent', 'Hello, World!')
+    )
+    .and('should have been rendered twice', ([_, renderCall]) => expect(renderCall).toHaveBeenCalledTimes(2));
+});
---
+++ packages/react-chain-of-responsibility/src/tests/preview/useRequest.test.tsx
@@ -0,0 +1,52 @@
+/** @jest-environment jsdom */
+/// <reference types="@types/jest" />
+
+import { scenario } from '@testduet/given-when-then';
+import { render } from '@testing-library/react';
+import React, { Fragment, memo } from 'react';
+
+import createChainOfResponsibility from '../../createChainOfResponsibilityAsRenderCallback';
+
+type Props = { readonly children?: never };
+
+scenario('stability test with changing request', bdd => {
+  bdd
+    .given('a TestComponent using chain of responsiblity', () => {
+      const {
+        Provider,
+        Proxy,
+        reactComponent,
+        types: _types,
+        useRequest
+      } = createChainOfResponsibility<string, Props>();
+
+      const MyComponent = memo(function MyComponent() {
+        const [request] = useRequest();
+
+        return <Fragment>{request}</Fragment>;
+      });
+
+      const middleware: readonly (typeof _types.middleware)[] = [
+        // With a fixed props, the <MyComponent> should be stable.
+        () => () => () => reactComponent(MyComponent)
+      ];
+
+      return function TestComponent({ request }: { readonly request: string }) {
+        return (
+          <Provider middleware={middleware}>
+            <Proxy request={request} />
+          </Provider>
+        );
+      };
+    })
+    .when('the component is rendered', TestComponent => render(<TestComponent request="Hello, World!" />))
+    .then('textContent should match', (_, { container }) =>
+      expect(container).toHaveProperty('textContent', 'Hello, World!')
+    )
+    .when('the component is re-rendered with a different request', (TestComponent, result) => {
+      result.rerender(<TestComponent request="Aloha!" />);
+
+      return result;
+    })
+    .then('textContent should match', (_, { container }) => expect(container).toHaveProperty('textContent', 'Aloha!'));
+});
