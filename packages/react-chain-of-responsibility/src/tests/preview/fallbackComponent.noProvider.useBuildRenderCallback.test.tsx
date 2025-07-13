4c7b50ec7e3b19f86455a39f71615075b7752e50
William Wong <compulim@hotmail.com>
2025-07-12 23:09:01 -0700

    Add test for useBuildRenderCallback outside of <Provider>

---
+++ packages/react-chain-of-responsibility/src/tests/preview/fallbackComponent.noProvider.useBuildRenderCallback.test.tsx
@@ -0,0 +1,32 @@
+/** @jest-environment jsdom */
+/// <reference types="@types/jest" />
+
+import { scenario } from '@testduet/given-when-then';
+import { render } from '@testing-library/react';
+import React, { Fragment } from 'react';
+
+import createChainOfResponsibility from '../../createChainOfResponsibilityAsRenderCallback';
+
+type Props = {
+  readonly children?: never;
+  readonly value: number;
+};
+
+function Fallback({ value }: Props) {
+  return <Fragment>Fallback ({value})</Fragment>;
+}
+
+scenario('rendering fallback component without <Provider>', bdd => {
+  bdd
+    .given('a TestComponent using chain of responsiblity', () => {
+      const { useBuildRenderCallback } = createChainOfResponsibility<void, Props>();
+
+      return function TestComponent() {
+        return useBuildRenderCallback()(undefined, { fallbackComponent: Fallback })?.({ value: 1 });
+      };
+    })
+    .when('the component is rendered', TestComponent => render(<TestComponent />))
+    .then('textContent should match', (_, { container }) =>
+      expect(container).toHaveProperty('textContent', 'Fallback (1)')
+    );
+});
