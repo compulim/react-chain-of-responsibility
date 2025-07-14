dd89f99bf18088b051b1d23e6580b4119b5e4b1e
William Wong <compulim@hotmail.com>
2025-07-14 02:16:03 -0700

    Simplify

--- packages/react-chain-of-responsibility/src/private/useMemoValueWithEquality.ts
+++ packages/react-chain-of-responsibility/src/private/useMemoValueWithEquality.ts
@@ -5,13 +5,7 @@ const NOT_INITIALIZED = Symbol();
 export default function useMemoValueWithEquality<T>(factory: () => T, equalityFn: (x: T, y: T) => boolean): T {
   const prevRef = useRef<T | typeof NOT_INITIALIZED>(NOT_INITIALIZED);
   const next: T = factory();
-  let current: T;
-
-  if (prevRef.current !== NOT_INITIALIZED && equalityFn(prevRef.current, next)) {
-    current = prevRef.current;
-  } else {
-    current = next;
-  }
+  const current: T = prevRef.current !== NOT_INITIALIZED && equalityFn(prevRef.current, next) ? prevRef.current : next;
 
   useEffect(() => {
     prevRef.current = current;
