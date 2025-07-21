8ba45d9ee2fde35263747ada6f8caae9b86f3579
@compulim <@compulim>
2025-07-21 07:00:46 +0000

    Bump scaffold

--- packages/react-chain-of-responsibility/tsup.config.ts
+++ packages/react-chain-of-responsibility/tsup.config.ts
@@ -4,9 +4,7 @@ export default defineConfig([
   {
     dts: true,
     entry: {
-      'react-chain-of-responsibility': './src/index.ts',
-      'react-chain-of-responsibility.fluentUI': './src/index.fluentUI.ts',
-      'react-chain-of-responsibility.preview': './src/index.preview.ts'
+      ['react-chain-of-responsibility'.split('/').reverse()[0]]: './src/index.ts'
     },
     format: ['cjs', 'esm'],
     sourcemap: true,
