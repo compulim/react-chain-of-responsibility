8efdd8d876c5baf037a378645a4e0a15b2dc91f1
William Wong <compulim@hotmail.com>
2025-07-26 13:30:33 -0700

    Add tests

--- packages/react-chain-of-responsibility/src/preview/tests/stability.changeMiddleware.test.tsx
+++ packages/react-chain-of-responsibility/src/preview/tests/stability.changeMiddleware.test.tsx
@@ -3,7 +3,7 @@
 
 import { scenario } from '@testduet/given-when-then';
 import { render } from '@testing-library/react';
-import React, { Fragment, memo } from 'react';
+import React, { Fragment } from 'react';
 
 import createChainOfResponsibility, { type InferMiddleware } from '../createChainOfResponsibilityAsRenderCallback';
 
@@ -17,34 +17,39 @@ scenario('stability test with changing middleware', bdd => {
 
       const alohaCall = jest.fn();
 
-      // Middleware prop change always re-render.
-      // Memoizing <Aloha> to focus on whether the component is rendering as waste.
-      const Aloha = memo(function Aloha() {
+      const Aloha = function Aloha() {
         alohaCall();
 
         return <Fragment>Aloha!</Fragment>;
-      });
+      };
+
+      // Changing middleware always call `reactComponent(...)` again and would result in wasted rendering.
+      // Until we do equality check on the return value of `reactComponent(...)` to see if they are different instances but same result.
+      // Otherwise, we need to cache the `reactComponent(Aloha)` for testing purpose.
+      // This way of testing it is preferred over `memo(Aloha)`.
+      // In production, web devs should do `memo(Aloha)` instead.
+      const renderAloha = reactComponent(Aloha);
 
       const helloWorldCall = jest.fn();
 
-      // Middleware prop change always re-render.
-      // Memoizing <HelloWorld> to focus on whether the component is rendering as waste.
-      const HelloWorld = memo(function HelloWorld() {
+      const HelloWorld = function HelloWorld() {
         helloWorldCall();
 
         return <Fragment>Hello, World!</Fragment>;
-      });
+      };
+
+      const renderHelloWorld = reactComponent(HelloWorld);
 
       const myProxyCall = jest.fn();
 
-      const MyProxy = memo(function MyProxy({ request }: { readonly request: number }) {
+      const MyProxy = function MyProxy({ request }: { readonly request: number }) {
         myProxyCall();
 
         // We are using `useBuildRenderCallback` for less memoization.
         const result = useBuildRenderCallback()(request)?.({});
 
         return result ? <Fragment>{result}</Fragment> : null;
-      });
+      };
 
       return Object.freeze({
         Aloha,
@@ -52,7 +57,8 @@ scenario('stability test with changing middleware', bdd => {
         HelloWorld,
         helloWorldCall,
         myProxyCall,
-        reactComponent,
+        renderAloha,
+        renderHelloWorld,
         TestComponent: function TestComponent({
           middleware
         }: {
@@ -66,8 +72,11 @@ scenario('stability test with changing middleware', bdd => {
         }
       });
     })
-    .when('the component is rendered', ({ HelloWorld, reactComponent, TestComponent }) =>
-      render(<TestComponent middleware={[() => () => () => reactComponent(HelloWorld)]} />)
+
+    // ---
+
+    .when('the component is rendered', ({ renderHelloWorld, TestComponent }) =>
+      render(<TestComponent middleware={[() => () => () => renderHelloWorld]} />)
     )
     .then('textContent should match', (_, { container }) =>
       expect(container).toHaveProperty('textContent', 'Hello, World!')
@@ -76,11 +85,14 @@ scenario('stability test with changing middleware', bdd => {
       expect(helloWorldCall).toHaveBeenCalledTimes(1)
     )
     .and('<MyProxy> should have been rendered once', ({ myProxyCall }) => expect(myProxyCall).toHaveBeenCalledTimes(1))
+
+    // ---
+
     .when(
       'the component is re-rendered with a different instance but same middleware',
-      ({ HelloWorld, reactComponent, TestComponent }, result) => {
+      ({ renderHelloWorld, TestComponent }, result) => {
         // Still rendering <HelloWorld>, but in a different middleware.
-        result.rerender(<TestComponent middleware={[() => () => () => reactComponent(HelloWorld)]} />);
+        result.rerender(<TestComponent middleware={[() => () => () => renderHelloWorld]} />);
 
         return result;
       }
@@ -94,10 +106,13 @@ scenario('stability test with changing middleware', bdd => {
     // Every time middleware change, it should invalidate `useBuildRenderCallback`.
     // In future, we may do more granular check to see if enhancer actually changed or not and reduce wasted rendering.
     .and('<MyProxy> should have been rendered once', ({ myProxyCall }) => expect(myProxyCall).toHaveBeenCalledTimes(2))
+
+    // ---
+
     .when(
       'the component is re-rendered with a different instance but same middleware',
-      ({ Aloha, reactComponent, TestComponent }, result) => {
-        result.rerender(<TestComponent middleware={[() => () => () => reactComponent(Aloha)]} />);
+      ({ renderAloha, TestComponent }, result) => {
+        result.rerender(<TestComponent middleware={[() => () => () => renderAloha]} />);
 
         return result;
       }
