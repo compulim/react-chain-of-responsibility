8904560989ebaffc2bbe20f9c8593133cf03a8df
@compulim <@compulim>
2025-12-31 23:57:08 +0000

    Bump scaffold

--- packages/react-chain-of-responsibility/tsup.config.ts
+++ packages/react-chain-of-responsibility/tsup.config.ts
@@ -1,23 +1,13 @@
-import { defineConfig, type Options } from 'tsup';
-
-const BASE_CONFIG: Options = {
-  dts: true,
-  entry: {
-    'react-chain-of-responsibility': './src/index.ts',
-    'react-chain-of-responsibility.preview': './src/index.preview.ts'
-  },
-  sourcemap: true
-};
+import { defineConfig } from 'tsup';
 
 export default defineConfig([
   {
-    ...BASE_CONFIG,
-    format: ['esm'],
+    dts: true,
+    entry: {
+      'react-chain-of-responsibility': './src/index.ts'
+    },
+    format: ['cjs', 'esm'],
+    sourcemap: true,
     target: 'esnext'
-  },
-  {
-    ...BASE_CONFIG,
-    format: ['cjs'],
-    target: 'es2019'
   }
 ]);
